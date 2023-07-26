7eac33c2efb4fcc9f2613bf9cff32a683a033b53
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-07-26 20:33:40 -0300

    Criação da rota perfil

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-
 def homepage():
     return "FakePinterest - Meu primeiro site Flask!"
 
+@app.route("/perfil")
+def perfil():
+    return "Perfil do Usuário"
 
 if __name__ == "__main__":
     app.run(debug=True)
