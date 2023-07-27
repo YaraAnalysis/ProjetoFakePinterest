cc2b1ca56e13ba3c5f2f3d2ed9cba11d74e1942c
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-07-26 21:03:23 -0300

    Usando templates html nas páginas

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "FakePinterest - Meu primeiro site Flask!"
+    return render_template("homepage.html")
 
 @app.route("/perfil")
 def perfil():
-    return "Perfil do Usuário"
+    return render_template("perfil.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
