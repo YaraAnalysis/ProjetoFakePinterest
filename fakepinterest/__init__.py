3733d7c84f214d21b6cacfccd8053b5cf5838325
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-07-31 19:35:01 -0300

    início do sistema de login e segurança

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,10 +1,16 @@
 #definir o nosso app, criar o nosso site
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"  #Aqui pode escolher outro DB, tipo Postgrees
+app.config["SECRET_KEY"] = "b405fce2e6d00c927b01de29aa9feeb8"
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
 
 from fakepinterest import routes
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,12 +1,14 @@
 #criar as rotas do nosso site (os links)
 from flask import render_template, url_for
 from fakepinterest import app
-
+from flask_login import login_required
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
+
 @app.route("/perfil/<usuario>")  # essa chave <>, sinaliza que é uma variável.
+@login_required
 def perfil(usuario):
     return render_template("perfil.html", usuario=usuario)
