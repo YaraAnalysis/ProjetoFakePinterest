637d70c0182b90901265c5a8b3c82e2f78d50b48
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-08-01 19:00:56 -0300

    Implementando funcionalidades de Login e Criar Conta - parte 1

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,20 +1,27 @@
 #criar as rotas do nosso site (os links)
 from flask import render_template, url_for
-from fakepinterest import app
+from fakepinterest import app, database, bcrypt
 from flask_login import login_required
 from fakepinterest.forms import FormLogin, FormCriarConta
+from fakepinterest.models import Usuario, Foto
 
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
-    formlogin = FormLogin()
-    return render_template("homepage.html", form=formlogin) #form antes do =, é o nome que a variável vai ter no html
+    form_login = FormLogin()
+    return render_template("homepage.html", form=form_login) #form antes do =, é o nome que a variável vai ter no html
 
 
 @app.route("/criarconta", methods=["GET", "POST"])
-def criarconta():
-    formcriarconta = FormCriarConta()
-    return render_template("criarconta.html", form=formcriarconta)
+def criar_conta():
+    form_criarconta = FormCriarConta()
+    if form_criarconta.validate_on_submit():
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = Usuario(username=form_criarconta.username.data,
+                          email=form_criarconta.email.data, senha=senha)
+        database.session.add(usuario) #adicionar usuário
+        database.session.commit()  #commitar as infos no banco
+    return render_template("criarconta.html", form=form_criarconta)
 
 
 @app.route("/perfil/<usuario>")  # essa chave <>, sinaliza que é uma variável.
