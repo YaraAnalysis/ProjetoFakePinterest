d195c64dd99c48ca3c1e95d70c0b3e7931893eae
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-07-31 23:53:31 -0300

    Implementando o formulário de login e criar conta

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -2,10 +2,19 @@
 from flask import render_template, url_for
 from fakepinterest import app
 from flask_login import login_required
+from fakepinterest.forms import FormLogin, FormCriarConta
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-    return render_template("homepage.html")
+    formlogin = FormLogin()
+    return render_template("homepage.html", form=formlogin) #form antes do =, é o nome que a variável vai ter no html
+
+
+@app.route("/criarconta", methods=["GET", "POST"])
+def criarconta():
+    formcriarconta = FormCriarConta()
+    return render_template("criarconta.html", form=formcriarconta)
 
 
 @app.route("/perfil/<usuario>")  # essa chave <>, sinaliza que é uma variável.
