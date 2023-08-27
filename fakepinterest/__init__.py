c55e159fddbbee2e2d7fa9e35bc01abb41ff164d
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-08-27 02:14:15 -0300

    Alteração do DB Postegresql

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -3,9 +3,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"  #Aqui pode escolher outro DB, tipo Postgrees
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"  #Aqui pode escolher outro DB, tipo Postgrees
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
 app.config["SECRET_KEY"] = "b405fce2e6d00c927b01de29aa9feeb8"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
